cc6f8973c9ecd39b3db12842add8b7dd0af538d7
Cary Hocker <chocker@gmail.com>
2018-08-13 11:43:11 -0500

    YOLO

--- src/main/java/com/arch/configserver/ConfigserverApplication.java
+++ src/main/java/com/arch/configserver/ConfigserverApplication.java
@@ -1,5 +1,6 @@
 package com.arch.configserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
